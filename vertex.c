ef5cd35833b316dd49fd776122525df33b6ffbe9
ruairidhm98 <2250079m@student.gla.ac.uk>
2019-06-23 09:31:50 +0100

    Moved all adjacency lists stuff into vertex files

--- vertex.c
+++ vertex.c
@@ -1,5 +1,4 @@
 #include "vertex.h"
-#include "adj_list.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,6 +8,21 @@ struct vertex {
   unsigned int index; // index into the graph array
   AdjList *adj_list;  // neighbours of the vertex
 };
+
+/* Node in adjacency list */
+struct adj_list_node {
+  Vertex *v;                  // the neigbour
+  unsigned int weight;        // weight of the edge between neighbours
+  struct adj_list_node *next; // pointer to next node
+};
+
+/* Adjacency list represented as a singly linked list */
+struct adj_list {
+  AdjListNode *head; // pointer to the head of the list
+  AdjListNode *tail; // pointer to the tail of the list
+  unsigned int size; // size counter
+};
+
 
 Vertex *v_init(unsigned int index) {
 
@@ -36,4 +50,84 @@ void v_add_adj_list(Vertex *v, Vertex *n, unsigned int weight) {
 void v_destroy(Vertex *v) {
   adj_list_destroy(v->adj_list);
   free(v);
+}
+
+AdjListNode *adj_node_init(Vertex *v, unsigned int weight) {
+
+  AdjListNode *node = (AdjListNode *)malloc(sizeof(AdjListNode));
+
+  if (node) {
+    node->v = v;
+    node->weight = weight;
+    node->next = NULL;
+  }
+
+  return node;
+}
+
+AdjListNode *adj_node_get_next(AdjListNode *n) { return n ? n->next : NULL; }
+
+Vertex *adj_node_get_vertex(AdjListNode *n) { return n ? n->v : NULL; }
+
+unsigned int adj_node_get_weight(AdjListNode *n) { return n ? n->weight : __INT_MAX__; }
+
+void adj_node_set_next(AdjListNode *n, AdjListNode *next) { n ? n->next = next : NULL; }
+
+AdjList *adj_list_init() {
+
+  AdjList *list = (AdjList *)malloc(sizeof(AdjList));
+
+  if (list) {
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+  }
+
+  return list;
+}
+
+int adj_list_add(AdjList *list, Vertex *v, unsigned int weight) {
+
+  if (list) {
+    AdjListNode *adj_node = adj_node_init(v, weight);
+    if (adj_node) {
+      if (!list->head) {
+        list->head = adj_node;
+        list->tail = list->head;
+      }
+      else {
+        adj_node_set_next(list->tail, adj_node);
+        list->tail = adj_node;
+      }
+      list->size++;
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+void adj_list_print(AdjList *list) {
+  
+  AdjListNode *cursor = list->head;
+
+  while (cursor) {
+    printf("%d ", v_get_index(adj_node_get_vertex(cursor)));
+    cursor = adj_node_get_next(cursor);
+  }
+  printf("\n");
+
+}
+
+void adj_list_destroy(AdjList *list) {
+  
+  AdjListNode *cursor = list->head, *temp;
+  
+  while (cursor) {
+    temp = cursor;
+    cursor = adj_node_get_next(cursor);
+    free(temp);
+  }
+  free(list);
+
 }
--- vertex.h
+++ vertex.h
@@ -5,6 +5,8 @@ typedef struct vertex Vertex;
 typedef struct adj_list AdjList;
 typedef struct adj_list_node AdjListNode;
 
+
+///////////////// VERTEX ///////////////////
 /* Returns a pointer to a vertex object if successfull, NULL otherwise */
 Vertex *v_init(unsigned int index);
 /* Returns the index of the vertex */
@@ -18,5 +20,28 @@ void v_add_adj_list(Vertex *v, Vertex *n, unsigned int weight);
 /* Destorys a verte object */
 void v_destroy(Vertex *v);
 
+////////////////// Adjacency list node ////////////
+
+/* Returns a pointer to an adjacency list node if successfull, NULL otherwise */
+AdjListNode *adj_node_init(Vertex *v, unsigned int weight);
+/* Return the next node */
+AdjListNode *adj_node_get_next(AdjListNode *n);
+/* Returns the vertex at the node */
+Vertex *adj_node_get_vertex(AdjListNode *n);
+/* Returns the weight of the edge */
+unsigned int adj_node_get_weight(AdjListNode *n);
+/* Set the neighbour of the adjacency list node */
+void adj_node_set_next(AdjListNode *n, AdjListNode *next);
+
+////////////////// Adjacency list ////////////////
+
+/* Returns a pointer to a adjacency list if successfull, NULL otherwise */
+AdjList *adj_list_init();
+/* Adds a new vertex to the adjacency list, returns 1/0 for success/failure */
+int adj_list_add(AdjList *list, Vertex *v, unsigned int weight);
+/* Prints the adjacency list */
+void adj_list_print(AdjList *list);
+/* Destorys an adjacency list */
+void adj_list_destroy(AdjList *list);
 
 #endif
