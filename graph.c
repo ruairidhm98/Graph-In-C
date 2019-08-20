daec11548d775d7c7b5a340055c949d31487bb54
ruairidhm98 <2250079m@student.gla.ac.uk>
2019-08-20 21:47:06 +0100

    Made misc functions in priority_queue inline to improve performance

--- graph.c
+++ graph.c
@@ -8,30 +8,35 @@ static void print_path(int *pred, int curr);
 
 /* Graph represented as an array of pointers, where each pointer
    is a pointer to a vertex object */
-struct graph {
+struct graph
+{
   Vertex **graph;    // the graph
   unsigned int size; // number of vertices
 };
 
-Graph *graph_init(unsigned int **matrix, unsigned int size) {
+Graph *graph_init(unsigned int **matrix, unsigned int size)
+{
 
   Graph *g = (Graph *)malloc(sizeof(Graph));
   unsigned int i, j, weight;
   AdjList *adj_list;
 
-  if (g) {
+  if (g)
+  {
     g->graph = (Vertex **)malloc(sizeof(Vertex *)*size);
-    if (g->graph) {
+    if (g->graph)
+    {
       g->size = size;
       // initialise each vertex
-      for (i = 0; i < size; i++) {
-        g->graph[i] = v_init(i);
-      }
+      for (i = 0; i < size; i++) g->graph[i] = v_init(i);
       // set up the adjacency lists
-      for (i = 0; i < size; i++) {
+      for (i = 0; i < size; i++)
+      {
         adj_list = v_get_adj_list(g->graph[i]);            
-        for (j = 0; j < size; j++) {
-          if (matrix[i][j]) {
+        for (j = 0; j < size; j++)
+        {
+          if (matrix[i][j])
+          {
             weight = matrix[i][j];
             adj_list_add(adj_list, g->graph[j], weight);
           }
@@ -44,9 +49,11 @@ Graph *graph_init(unsigned int **matrix, unsigned int size) {
   return NULL;
 }
 
-void graph_print(Graph *g) {
+void graph_print(Graph *g)
+{
 
-  for (unsigned int i = 0; i < g->size; i++) {
+  for (unsigned int i = 0; i < g->size; i++)
+  {
     printf("----------\n");
     printf("Vertex %d\n", v_get_index(g->graph[i]));
     adj_list_print(v_get_adj_list(g->graph[i]));
@@ -59,14 +66,17 @@ unsigned int dijktras(Graph *g, unsigned int start, unsigned int dest) {
   return 0;
 }*/
 
-void graph_destroy(Graph *g) {
+void graph_destroy(Graph *g)
+{
 
   AdjList *adj_list;
-  for (unsigned int i = 0; i < g->size; i++) {
+  for (unsigned int i = 0; i < g->size; i++)
+  {
     adj_list = v_get_adj_list(g->graph[i]);
     adj_list_destroy(adj_list);
   }
   free(g->graph);
   free(g);
 
+  return 0;
 }
--- priority_queue.c
+++ priority_queue.c
@@ -31,7 +31,8 @@ static void swap(int *ele, int i, int j);
 static int pq_realloc(PriorityQueue *pq);
 
 /* Priority queue implemented with a min pq */
-struct pqueue {
+struct pqueue
+{
   Vertex **queue;                 // the queue
   unsigned int size;              // size counter
   unsigned int capacity;          // capacity of heap
@@ -39,12 +40,15 @@ struct pqueue {
 };
 
 
-PriorityQueue *pq_init(unsigned int capacity) { 
+PriorityQueue *pq_init(unsigned int capacity)
+{ 
 
   PriorityQueue *pq = (PriorityQueue *)malloc(sizeof(PriorityQueue));
-  if (pq) {
+  if (pq)
+  {
     pq->queue = (Vertex **)malloc(sizeof(Vertex *)*capacity);
-    if (pq->queue) {
+    if (pq->queue)
+    {
       pq->size = 0;
       pq->capacity = capacity;
       return pq;
@@ -55,12 +59,13 @@ PriorityQueue *pq_init(unsigned int capacity) {
 }
 
 
-void pq_add(PriorityQueue *pq, Vertex *v) {
+void pq_add(PriorityQueue *pq, Vertex *v)
+{
 
-  if (pq) {
+  if (pq)
+  {
     if (pq->size == pq->capacity)
-      if (!pq_realloc(pq))
-        return;  
+      if (!pq_realloc(pq)) return;  
     pq->queue[pq->size++] = v;
     heapfiy_up(pq);
   }
@@ -68,7 +73,8 @@ void pq_add(PriorityQueue *pq, Vertex *v) {
 }
 
 
-Vertex *pq_poll(PriorityQueue *pq) {
+Vertex *pq_poll(PriorityQueue *pq)
+{
   
   Vertex *root = pq->queue[0];
   pq->queue[0] = pq->queue[--pq->size];
@@ -82,44 +88,53 @@ void pq_decrease(unsigned int index, unsigned int new_prio) {
 
 }*/
 
-void pq_print(PriorityQueue *pq) {
-  if (pq) {
+void pq_print(PriorityQueue *pq)
+{
+  if (pq)
+  {
     printf("[ ");
-    for (unsigned int i = 0; i < pq->size; i++) {
+    for (unsigned int i = 0; i < pq->size; i++)
+    {
       printf("%d ", pq->queue[i]);
     }
     printf("]\n");
   }
+
 } 
 
-void pq_destroy(PriorityQueue *pq) {
+void pq_destroy(PriorityQueue *pq)
+{
   free(pq->queue);
   free(pq);
 }
 
 /* Bubbles elements up */
-static void heapfiy_up(PriorityQueue *pq) {
+static void heapfiy_up(PriorityQueue *pq)
+{
 
   int index;
 
   index = pq->size - 1;
   /* Loop until correct position has been found */
-  while (has_parent(index) && get_parent(pq, index) > pq->queue[index]) {
+  while (has_parent(index) && get_parent(pq, index) > pq->queue[index])
+  {
     /* Swaps with parent node if parent is greater than node being bubbled */
     swap(pq->queue, get_parent_index(index), index);
     index = get_parent_index(index);
   }
+
 }
 
 /* Bubbles elements down */
-static void heapfiy_down(PriorityQueue *pq) {
+static void heapfiy_down(PriorityQueue *pq)
+{
 
   int index, smaller_child_index;
 
   index = 0;
   /* Loop until node is in correct place */
-  while (has_left_child(pq, index)) {
-
+  while (has_left_child(pq, index))
+  {
     smaller_child_index = get_left_child_index(index);
     /* The right child is smaller */
     if (has_right_child(pq, index) &&
@@ -138,79 +153,75 @@ static void heapfiy_down(PriorityQueue *pq) {
 
 
 /* Helper function, gets index of left child */
-static unsigned int get_left_child_index(int parent_index) {
+static inline unsigned int get_left_child_index(int parent_index) 
+{
   return 2 * parent_index + 1;
 }
 
 /* Helper function, gets index of right child */
-static unsigned int get_right_child_index(int parent_index) {
+static inline unsigned int get_right_child_index(int parent_index)
+{
   return 2 * parent_index + 2;
 }
 
 /* Helper function, gets index of parent node */
-static unsigned int get_parent_index(int child_index) { return (child_index - 1) / 2; }
+static inline unsigned int get_parent_index(int child_index)
+{ 
+  return (child_index - 1) / 2;
+}
 
 /* Helper function, checks if parent node has a left child */
-static unsigned int has_left_child(PriorityQueue *pq, int index) {
+static inline unsigned int has_left_child(PriorityQueue *pq, int index)
+{
   return get_left_child_index(index) < pq->size;
 }
 
 /* Helper function, checks if parent node has a right child */
-static unsigned int has_right_child(PriorityQueue *pq, int index) {
+static inline unsigned int has_right_child(PriorityQueue *pq, int index)
+{
   return get_right_child_index(index) < pq->size;
 }
 
 /* Helper function, checks if a given node has a parent */
-static unsigned int has_parent(int index) { return get_parent_index(index) >= 0; }
+static inline unsigned int has_parent(int index)
+{ 
+  return get_parent_index(index) >= 0;
+}
 
 /* Helper function, gets the left child of a parent node */
-static Vertex get_left_child(PriorityQueue *pq, int index) {
+static inline Vertex get_left_child(PriorityQueue *pq, int index)
+{
   return pq->queue[get_left_child_index(index)];
 }
 
 /* Helper function, gets the right child of a parent node */
-static Vertex get_right_child(PriorityQueue *pq, int index) {
+static inline Vertex get_right_child(PriorityQueue *pq, int index)
+{
   return pq->queue[get_right_child_index(index)];
 }
 
 /* Helper function, gets the parent of a given node */
-static Vertex get_parent(PriorityQueue *pq, int index) {
+static inline Vertex get_parent(PriorityQueue *pq, int index)
+{
   return pq->queue[get_parent_index(index)];
 }
 
 /* Helper function, swaps two elements */
-static void swap(int *ele, int i, int j) {
-
+static inline void swap(int *ele, int i, int j)
+{
   int temp = ele[i];
   ele[i] = ele[j];
   ele[j] = temp;
-
 }
 
 /* Helper function, reallocates memory for heap if it becomes full */
-static int pq_realloc(PriorityQueue *pq) {
-  if (pq) {
+static int pq_realloc(PriorityQueue *pq)
+{
+  if (pq)
+  {
     pq->capacity = 2 * pq->capacity;
     pq->queue = (int *)realloc(pq->queue, pq->capacity);
-    if (pq->queue)
-      return 1;
+    if (pq->queue) return 1;
   }
-  return 0;
-}
-
-int main() {
-
-  PriorityQueue *pq = pq_init(10);
-
-  for (int i = 0; i < 11; i++) {
-    pq_add(pq, rand() % 100);
-  }
-  pq_print(pq);
-  while (pq->size) {
-    printf("%d ", pq_poll(pq));
-  }
-  printf("\n");
-
-
   return 0;
 }
